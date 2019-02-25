4671094d13e3a2c7dac537a3e5273c34fc42a07b
q389491729 <389491729@qq.com>
2019-02-25 17:18:31 +0800

    写好了主页的框架

--- router/router.js
+++ router/router.js
@@ -4,7 +4,8 @@ const express = require('express'),
       user = require('../model/user.js') //对user操作的逻辑结构
       
 //监听引导页面
-router.get('/',( req , res ) => {res.send('这里是主页')})
+router.get('/',( req , res ) => {res.render('index',{title: "项目流程体验平台",login: req.session.login,
+user: req.session.user,})})
 //监听注册页面
 router.get('/reg',( req , res ) => { res.render('reg',{title: "注册用户"})})
       .post('/reg',user.reg)
